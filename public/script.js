998fb86a1763ad27c5fc476f0d35ed08451ccfe7
Supratim Saha <supratim1703@gmail.com>
2020-05-05 14:25:06 +0530

    bug fixes

--- public/script.js
+++ public/script.js
@@ -17,21 +17,49 @@ $(document).ready(function(){
 
     $(".btn1").on('click', function(){
         $(".modal-title").text("Donate Groceries");
+        $(".modal-body #InputName2").val('Grocery Donation')
     });
     $(".btn2").on('click', function(){
         $(".modal-title").text("Donate Medical");
+        $(".modal-body #InputName2").val('Medical Donation')
     });
     $(".btn3").on('click', function(){
         $(".modal-title").text("Donate Food Packets");
+        $(".modal-body #InputName2").val('Food Donation')
     });
     $(".btn4").on('click', function(){
         $(".modal-title").text("Donate Clothes");
+        $(".modal-body #InputName2").val('Cloth Donation')
     });
     $(".btn5").on('click', function(){
         $(".modal-title").text("Donate Books");
+        $(".modal-body #InputName2").val('Book Donation')
     });
 
     $('.modal').on('hidden.bs.modal', function (e) {
         $(this).find("input,textarea").val('').end();
       })
 });
+
+$('#donation_done').click(function(){
+    alert('Donation successfull recorded. Our team will soon reach you. Please check your email.');
+})
+
+$('#team_contact').click(function(){
+    alert('Our team has been informed. Please check your email');
+})
+
+//Simulating navbar movements
+let prev_nav_flag = $('.navbar-nav li:first-child a');
+prev_nav_flag.toggleClass('active');
+
+//for toggling navbar active class when navbar links are clicked
+$(".navbar-nav li a").click(function(){
+    prev_nav_flag.toggleClass('active');
+    prev_nav_flag = $(this);
+    $(this).toggleClass('active');
+});
+
+
+
+
---
+++ server.js
@@ -0,0 +1,106 @@
+const express= require('express');
+const path= require('path');
+const nodemailer= require('nodemailer');
+
+const app= express();
+let port= process.env.PORT || 3000 ;
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+let ouremail= 'shareforindiahelpinghands@gmail.com';
+let ourpassword= 'covidrelief'
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: ouremail,
+      pass: ourpassword
+    }
+});
+
+app.post('/donate_request',(req,res)=>{
+    let sub= req.body.subject;
+    let email= req.body.emailadd.trim();
+    let txt= `Name: ${req.body.name}\nMobile number: ${req.body.mobileno}\nEmail id: ${req.body.emailadd}\nAddress: ${req.body.addressin}\nQuantity(approx.): ${req.body.quantityapprox}`;
+    
+    console.log(sub);
+    console.log(email);
+    console.log(txt);
+
+    let mailOptions = {
+        from: ouremail,
+        to: ouremail,
+        subject: sub,
+        text: txt
+    };
+
+    let mailOptions2 = {
+        from: ouremail,
+        to: email,
+        subject: "Thank you for "+ sub,
+        text: `We heartly thank you for your donation. Your donation records:\n${txt}\n\n#StayHome #StaySafe`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+
+    transporter.sendMail(mailOptions2, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+
+    res.redirect('back');
+})
+
+app.post('/contactus',(req,res)=>{
+    let name= req.body.name;
+    let mobile= req.body.mobile;
+    let email= req.body.email.trim();
+    let msg= req.body.msg;
+
+    let mailOptions = {
+      from: ouremail,
+      to: ouremail,
+      subject: 'Contact US',
+      text: `Name: ${name}\nMobile: ${mobile}\nEmail: ${email}\nMessage: ${msg}`
+    };
+
+    let mailOptions2 = {
+        from: ouremail,
+        to: email,
+        subject: 'Thank You',
+        text: `Thank You for contacting us. Our team will soon review your message.`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+
+    transporter.sendMail(mailOptions2, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+
+    res.redirect('back');
+
+})
+
+app.listen(port,()=>{console.log('Server listening')});
